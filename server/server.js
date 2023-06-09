be60de69cef8ff2e8debbfbea62aeaaf808a9a05
Mickie <mickystn10@gmail.com>
2023-06-09 12:21:15 +0700

    add

--- server/server.js
+++ server/server.js
@@ -1,14 +1,9 @@
 
 const express = require('express');
 const app = express();
-const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
-app.get('/test',(req,res)=>{
-    res.send("test");
-})
-
 app.listen(8121,()=>{
-    console.log("Run at 213123");
+    log
 });
--- server2/index.js
+++ server2/index.js
@@ -4,9 +4,13 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+const {xy1}= require('./example/1.js');
+const {xy2}= require('./example/2.js');
+const {errors}= require('./example/3.js');
+const {xy4} = require('./example/4.js');
+const {gauss} = require('./example/gauss.js');
 app.get("/data",(req,res)=>{
-  res.send("test");
+  res.send(xy1);
 })
 
 app.listen(3001, () => {
